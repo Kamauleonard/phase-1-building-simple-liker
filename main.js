199a776bcceee21f1dd31bcf0e5f3f19aa797dfd
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-21 10:12:26 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-// Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal');
+  modal.classList.add('hidden');
+  
+  const posts = document.querySelectorAll('.media-post');
+  posts.forEach(post => {
+    const like = post.querySelector('.like');
+    
+    like.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (like.classList.contains('activated-heart')) {
+            like.classList.remove('activated-heart');
+            like.innerText = EMPTY_HEART;
+          } else {
+            like.classList.add('activated-heart');
+            like.innerText = FULL_HEART;
+          }
+        })
+        .catch((error) => {
+          modal.classList.remove('hidden');
+          const modalMessage = document.getElementById('modal-message');
+          modalMessage.innerText = error;
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 
 
 
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
